359d36cfce358a318243511e001bd5f83198e478
OrionGift3 <ppexburt@gmail.com>
2024-09-23 20:21:04 +0400

    reeeeeeeeeeeeee

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState(null);
+
+  const base = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(base);
+        if (!response.ok) throw new Error("Network response was not ok");
+
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error("This is my error:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const fetchUserById = async (id) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+      );
+      if (!response.ok) throw new Error("Network response was not ok");
+
+      const data = await response.json();
+      setUser(data);
+    } catch (error) {
+      console.error("This is my error:", error);
+    }
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Users List</h1>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id} onClick={() => fetchUserById(user.id)}>
+              {user.name}
+            </li>
+          ))}
+        </ul>
+        {user && (
+          <div>
+            <h1>User Data</h1>
+            <p>
+              <strong>Name:</strong> {user.name}
+            </p>
+            <p>
+              <strong>Email:</strong> {user.email}
+            </p>
+            <p>
+              <strong>Address:</strong> {user.address.city},{" "}
+              {user.address.street}
+            </p>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
